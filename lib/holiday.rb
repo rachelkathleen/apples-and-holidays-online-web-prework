b23ba2fd5e53e9f62786a881a5849bc6728e6d5d
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 02:54:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,11 +39,11 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   bbq_array = []
-  holiday_hash.collect do |season, holiday| 
+  holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, supply|
       holiday if supply.include?("BBQ")
         bbq_array << holiday
-end
-end
+      end
+    end
   bbq_array
 end
