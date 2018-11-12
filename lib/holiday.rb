0b7cb6270253aed5cec40581c1e6b04fe9625484
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 02:35:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,9 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, holiday|
   holiday.each do |supplies|
-    puts "#{attribute}: #{value}"
+    puts
+    "#{season}:
+      #{holiday}: #{supplies}"
   end
 end
   # iterate through holiday_hash and print items such that your readout resembles:
