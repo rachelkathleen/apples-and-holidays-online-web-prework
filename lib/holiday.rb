b88faa13508942c20d2da5988aa0cb77c65711fc
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 02:25:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,6 +31,7 @@ def all_winter_holiday_supplies(holiday_hash)
     winter_supplies.flatten
 end
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -46,5 +47,4 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
