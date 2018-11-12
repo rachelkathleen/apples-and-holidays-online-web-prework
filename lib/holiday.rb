6a55a3bb9584c1660caf805bfdb100f476112e52
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 02:13:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,9 +21,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash << season
   holiday_hash[season] << holiday_name
   holiday_hash[season][holiday_name] << supply_array
-end
-end
-end
+  
 holiday_hash
 end
 
